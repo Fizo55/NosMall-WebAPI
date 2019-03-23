7f5c700bf7f4bdc3d3271bdc6eec2311f642ab69
Fizo55 <hugomacees56000@gmail.com>
2019-03-23 20:47:02 +0100

    [FIX] Fizo55/NosMall#9

---
+++ WebAPI/Controllers/BuyController.cs
@@ -0,0 +1,35 @@
+﻿using OpenNos.Master.Library.Client;
+using OpenNos.Master.Library.Data;
+using System;
+using System.Configuration;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class BuyController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Index(ItemModel model)
+        {
+            try
+            {
+                MallServiceClient.Instance.Authenticate(ConfigurationManager.AppSettings["MasterAuthKey"]);
+                MallServiceClient.Instance.SendItem(model.CharacterId, new MallItem()
+                {
+                    ItemVNum = model.ItemVNum,
+                    Amount = model.Amount,
+                    Rare = model.Rare,
+                    Upgrade = model.Upgrade
+                });
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[INFO] {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second} - Item send to the CharacterId : {model.CharacterId}");
+                return Ok($"Item succeffuly send to the character {model.CharacterId}");
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
