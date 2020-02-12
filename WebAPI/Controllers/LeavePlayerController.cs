f25e1a3beefd980f2a5a2b6f43194a893405220a
Fizo55 <hugomacees56000@gmail.com>
2020-02-12 15:02:16 +0100

    Add leave player controller

---
+++ WebAPI/Controllers/LeavePlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class LeavePlayerController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Index([FromBody] LeavePlayerModel leavePlayerModel)
+        {
+            if (leavePlayerModel.VerificationToken != ConfigurationManager.AppSettings["VerificationToken"]) return BadRequest();
+            
+            if (CommunicationServiceClient.Instance.Authenticate(ConfigurationManager.AppSettings["MasterAuthKey"]))
+            {
+                CommunicationServiceClient.Instance.DisconnectAccount(leavePlayerModel.AccountId);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ WebAPI/Models/LeavePlayerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class LeavePlayerModel
+    {
+        public string VerificationToken { get; set; }
+        
+        public long AccountId { get; set; }
+    }
+}
